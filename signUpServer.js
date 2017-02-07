9022ddf7d4aed8f33561991e7dbf776f1110a08f
Olav Poppe <olavpo@ifi.uio.no>
2017-02-07 10:46:33 +0100

    Updated structure, will use express

--- signUpServer.js
+++ signUpServer.js
@@ -1,26 +1,31 @@
 (function(){
-	var Q = require('q');
-
-	var conf = require('./conf/configuration.json');
-	var d2 = require('./bin/d2.js');
-
-	run();
-
-	/**
-	 * Start the server
-	 */
-	function run() {
-		
-	}
-
-
-	function template() {
-		var deferred = Q.defer();
-
-		deferred.resolve(true);
-				
-		return deferred.promise;
+	var express = require('express');
+	var bodyParser = require('body-parser');
 	
+	//Set up express app	
+	var app = express();
+
+	//Use body-parser
+	app.use( bodyParser.json() );
+
+
+	//Log all requests
+	var logger = function(req, res, next) {
+		console.log(req.method + " " + req.originalUrl);
+		if (req.method === 'POST') console.log(req.body);
+		next();
 	}
+	app.use(logger);
+
+
+	//Use express for static files
+	app.use(express.static('public'))
+	
+
+	//Start server on port 8089
+	app.listen(8089, function () {
+		console.log('Listening on port 8089!');
+	});
+
 
 }());
