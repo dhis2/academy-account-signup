508e28ee9fffaee76c1ed68289b3e9840f421290
olavpo <olavpo@ifi.uio.no>
2021-07-02 13:27:19 +0200

    Removed functionality for checking user results

--- signUpServer.js
+++ signUpServer.js
@@ -19,7 +19,7 @@
 
 
 	//Use express to serve static files, i.e. the frontend registration form
-	app.use(express.static('public'));
+	//app.use(express.static('public'));
 
 
 	//Use body-parser for parsing json
@@ -86,30 +86,6 @@
 	});
 
 
-	//"API" for checking results
-	app.post('/check/api', function (req, res) {
-		var valid = req.body.hasOwnProperty('email');
-		if (!req.body.hasOwnProperty('email')) {
-			res.statusCode = 400;
-			return res.send('Bad request ("email" property required)');
-		}
-		if (!req.body.hasOwnProperty('url')) {
-			res.statusCode = 400;
-			return res.send('Bad request ("url" property required)');
-		}
-
-		console.log(new Date() + " Checking " + req.body.email);
-
-		resultForUser(req.body).then(function (success) {
-			console.log(new Date() + " Result " + req.body.email + ": " + success.message);
-			res.statusCode = 201;
-			return res.send(success.message);
-		});
-	});
-
-
-
-
 	//Make accounts (invites)
 	//Call functions depending on type(s) of accounts to be created
 	function makeAccounts(userInfo) {
@@ -153,7 +129,6 @@
 		return deferred.promise;
 
 	}
-
 
 	//Make account (invite) for data use, i.e. just an account with role and groups
 	function makeDefaultAccount(userInfo, definition) {
@@ -246,8 +221,6 @@
 	//Make account (invite) for data use, i.e. account with a private "subtree" in the hierarchy, an empty data set and a user role
 	function makeAggregateFundamentalsAccount(userInfo, definition) {
 		var deferred = Q.defer();
-
-
 
 		//Check for duplicate
 		d2.get("/api/users.json?filter=email:eq:" + userInfo.email, definition.server).then(function (data) {
@@ -349,125 +322,6 @@
 	}
 
 
-	//Check results
-	function resultForUser(userInfo) {
-
-		//Makes promises for account creation
-		var deferred = Q.defer();
-		var promises = [];
-		var config;
-		
-		//Get the configuration from the request 
-		if (userInfo.hasOwnProperty('url') && userInfo.url) {
-			var url = userInfo.url;
-
-			//Get configuration for the requested url
-			config = getConfType(url, 'result');
-
-		}
-	
-		var assignment = userInfo.assignment ? userInfo.assignment : false;
-		if (assignment) {
-			switch (assignment) {
-				case 1:
-					promises.push(resultCustomisation(userInfo.email, config));
-				//More as needed...
-			}
-		}
-
-		//Check result of account creation, return true if all were successful
-		var result = true;
-		Q.all(promises).then(function (results) {
-			for (var i = 0; i < results.length; i++) {
-				result = result && results[i];
-			}
-
-			deferred.resolve(result);
-		})
-
-
-		return deferred.promise;
-
-	}
-
-	//Check result of customisation exercise
-	function resultCustomisation(email, definition) {
-		var deferred = Q.defer();
-
-		//Check for duplicate
-		d2.get("/api/users.json?filter=email:eq:" + email + '&fields=created,organisationUnits[level,children,id]&paging=false', definition.server).then(function (data) {
-
-			if (data.users.length === 0) {
-				deferred.resolve({ "success": false, "message": "Account with email " + email + " not found." });
-				return;
-			}
-
-
-			//Get oldest (first created) user
-			var currentUser = data.users[0];
-			for (var i = 1; i < data.users.length; i++) {
-				if (currentUser.created > data.users[i].created) currentUser = data.users[i];
-			}
-
-			var currentOrgunit = currentUser.organisationUnits[0];
-			for (var i = 1; i < currentUser.organisationUnits.length; i++) {
-				if (currentOrgunit.level > currentUser.organisationUnits[i].level) currentOrgunit = currentUser.organisationUnits[i];
-			}
-
-			//User have not created a new orgunit (child)
-			if (currentOrgunit.children.length === 0) {
-				deferred.resolve({ "success": false, "message": "No orgunits have been created." });
-				return;
-			}
-
-			//Get all orgunits and children below the "root" orgunit
-			d2.get("/api/organisationUnits.json?filter=path:like:" + currentOrgunit.id + "/&fields=[name,id,dataSets]&paging=false", definition.server).then(function (data) {
-
-				var dataSetExists = {};
-
-				var dataSetIds = [];
-				var orgunitIds = [];
-				for (var i = 0; i < data.organisationUnits.length; i++) {
-					var ou = data.organisationUnits[i];
-					orgunitIds.push(ou.id);
-					for (var j = 0; j < ou.dataSets.length; j++) {
-						if (!dataSetExists[ou.dataSets[j].id]) {
-							dataSetExists[ou.dataSets[j].id]
-							dataSetIds.push(ou.dataSets[j].id);
-						}
-					}
-				}
-
-				if (dataSetIds.length < 1) {
-					deferred.resolve({ "success": false, "message": "No data set assigned to orgunit children." });
-					return;
-				}
-
-				var url = '/api/dataValueSets.json?startDate=2000-01-01&endDate=2025-01-01';
-				url += '&dataSet=' + dataSetIds.join('&dataSet=');
-				url += '&orgUnit=' + orgunitIds.join('&orgUnit=');
-
-				d2.get(url, definition.server).then(function (data) {
-					if (data && data.dataValues && data.dataValues.length > 0) {
-						deferred.resolve({ "success": true, "message": "Result OK" });
-					}
-					else {
-						deferred.resolve({ "success": false, "message": "No data values." });
-					}
-				});
-			});
-		});
-
-		return deferred.promise;
-	}
-
-
-	//Start server on port 8099
-	app.listen(8099, function () {
-		console.log('Listening on port 8099!');
-	});
-
-
 	//Get configuration based on url
 	function getConf(url) {
 		for (var i = 0; i < conf.inviteConfigs.length; i++) {
@@ -476,14 +330,11 @@
 
 		return false;
 	}
-	
-	//Get configuration based on url and type
-	function getConfType(url, type) {
-		for (var i = 0; i < conf.inviteConfigs.length; i++) {
-			if (conf.inviteConfigs[i].server.url == url && conf.inviteConfigs[i].type == type) return conf.inviteConfigs[i];
-		}
 
-		return false;
-	}
+	//Start server on port 8099
+	app.listen(8099, function () {
+		console.log('Listening on port 8099!');
+	});
+
 
 }());
